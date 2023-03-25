0d77835b25b561225b9cdae43d9ef9ffbc62e1e8
Alex Stan <stan.alexandru@ichb.ro>
2023-03-25 11:49:15 +0200

    move nix-software-center into the flake.nix
    Signed-off-by: Alex Stan <stan.alexandru@ichb.ro>

--- flake.nix
+++ flake.nix
@@ -1,11 +1,15 @@
 {
   description = "My NixOS system configuration";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-software-center.url = "github:vlinkz/nix-software-center";
+  };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations.ultrapc = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
       modules = [
         ./ultrapc/configuration.nix
       ];
