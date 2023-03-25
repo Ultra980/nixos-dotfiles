45174312aab944458a6177c0c7a44c4513bb6f90
Alex Stan <90788596+Ultra980@users.noreply.github.com>
2023-03-25 09:20:16 +0200

    initial commit
    Signed-off-by: Alex Stan <stan.alexandru@ichb.ro>

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "My NixOS system configuration";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.ultrapc = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
