8529f8bd45fd874d3d8db0f2a353ac8ae98bc91d
Alex Stan <90788596+Ultra980@users.noreply.github.com>
2023-03-27 18:21:54 +0300

    changes
    Signed-off-by: Alex Stan <stan.alexandru@ichb.ro>

--- ultrapc/home.nix
+++ ultrapc/home.nix
@@ -1,12 +1,52 @@
-{pkgs, ...}: {
+{ pkgs, inputs, ... }: 
+let
+  nix-software-center = inputs.nix-software-center.packages.${pkgs.system}.nix-software-center;
+  doom-emacs = inputs.nix-doom-emacs.packages.${pkgs.system}.default;
+in {
     home = {
       username = "ultra";
       homeDirectory = "/home/ultra/";
       stateVersion = "22.11";
       packages = with pkgs; [
-        # neovim
-        # bat
+        firefox
+        kate
+        distrobox
+        fish
+        nushell
+        git
+        bat
+        gnupg1
+        zoom-us
+        google-chrome
+        zoxide
+        starship
+        atuin
+        clang
+        gcc
+        exa
+        gnumake
+        plasma-browser-integration
+        libsForQt5.bismuth
+        whatsapp-for-linux
+        signal-desktop
+        nodejs
+        onlyoffice-bin
+        drawio
+        xclip
+        steam
+        packagekit
+        armcord
+        packagekit
+        rnote
+        obsidian
+        gnome-obfuscate
+        microsoft-edge
+        doom-emacs
+        ripgrep
       ];
     };
-    programs.home-manager.enable = true;
+    programs = {
+      home-manager.enable = true;
+      # fish.enable = true;
+    };
 }
