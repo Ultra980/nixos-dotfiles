354d386342c0a266549a1dc0112d9c95bf7030a2
Alex S <stan.alexandru@ichb.ro>
2023-05-06 18:16:54 +0000

    organized
    Signed-off-by: Alex S <stan.alexandru@ichb.ro>

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
             system = "x86_64-linux";
             specialArgs = { inherit inputs; };
             modules = [
-                ./ultrapc/configuration.nix
+                ./hosts/ultrapc/configuration.nix
                 {
                     environment.etc."nix/inputs/nixpkgs".source = nixpkgs.outPath;    
                     nix.nixPath = [ "nixpkgs=/etc/nix/inputs/nixpkgs" ];
